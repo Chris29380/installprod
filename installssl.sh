cf1ad41f7f8f442c12d909453101eadca48cadb6
CDT Dev <148004410+Chris29380@users.noreply.github.com>
2024-12-02 08:53:22 +0100

    debug

--- installssl.sh
+++ installssl.sh
@@ -23,8 +23,10 @@ install_ssl(){
     if [ "${urlmachine}" != "" ]; then
         certbot --nginx -d $urlmachine
         sleep 5
-        if [ -d "/etc/letsencrypt/live/$urlmachine/" ]; then
-            VALID=1
+        if [ ! -d "/etc/letsencrypt/live/$urlmachine/" ]; then
+            echo
+            echo -e "\n${COLOR1}SSl Certification error, stop process... ${NC}"
+            exit 0
         fi
     else
         echo
@@ -37,8 +39,10 @@ install_ssl(){
     if [ "${urlplay}" != "" ]; then
         certbot --nginx -d $urlplay
         sleep 5
-        if [ -d "/etc/letsencrypt/live/$urlplay/" ]; then
-            VALID=2
+        if [ ! -d "/etc/letsencrypt/live/$urlmachine/" ]; then
+            echo
+            echo -e "\n${COLOR1}SSl Certification error, stop process... ${NC}"
+            exit 0
         fi
     else
         echo
@@ -50,8 +54,10 @@ install_ssl(){
     if [ "${urltx}" != "" ]; then
         certbot --nginx -d $urltx
         sleep 5
-        if [ -d "/etc/letsencrypt/live/$urltx/" ]; then
-            VALID=3
+        if [ ! -d "/etc/letsencrypt/live/$urlmachine/" ]; then
+            echo
+            echo -e "\n${COLOR1}SSl Certification error, stop process... ${NC}"
+            exit 0
         fi
     else
         echo
@@ -63,8 +69,10 @@ install_ssl(){
     if [ "${urlphp}" != "" ]; then
         certbot --nginx -d $urlphp
         sleep 5
-        if [ -d "/etc/letsencrypt/live/$urlphp/" ]; then
-            VALID=4
+        if [ ! -d "/etc/letsencrypt/live/$urlmachine/" ]; then
+            echo
+            echo -e "\n${COLOR1}SSl Certification error, stop process... ${NC}"
+            exit 0
         fi
     else
         echo
@@ -76,8 +84,10 @@ install_ssl(){
     if [ "${urlpanel}" != "" ]; then
         certbot --nginx -d $urlpanel
         sleep 5
-        if [ -d "/etc/letsencrypt/live/$urlpanel/" ]; then
-            VALID=5
+        if [ ! -d "/etc/letsencrypt/live/$urlmachine/" ]; then
+            echo
+            echo -e "\n${COLOR1}SSl Certification error, stop process... ${NC}"
+            exit 0
         fi
     else
         echo
@@ -91,80 +101,73 @@ install_ssl(){
     echo
     echo -e "\n${COLOR2} Nginx Copy Configuration Files done ! ... ${NC}"
     echo
-    if [ "$VALID" -eq 5 ]; then
-        echo -e "\n${COLOR3}Fivem server port ? ${NC}(default: 30120)"    
-        read -p "Port Cfx Server : " cfxport
-        echo
-        echo -e "\n${COLOR3}txAdmin Port ? ${NC}(default:40120)"
-        read -p "txAdmin port: " txport 
+    echo -e "\n${COLOR3}Fivem server port ? ${NC}(default: 30120)"    
+    read -p "Port Cfx Server : " cfxport
+    echo
+    echo -e "\n${COLOR3}txAdmin Port ? ${NC}(default:40120)"
+    read -p "txAdmin port: " txport 
 
-        if [ "${urlmachine}" != "" ]; then
-            sed -i "s/machine_url_here/$urlmachine/g" ./nginxfiles/default
-        else
-            echo
-            echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
-            exit 0
-        fi
-        if [ "${urlplay}" != "" ]; then
-            sed -i "s/play_url_here/$urlplay/g" ./nginxfiles/prodssl.conf
-        else
-            echo
-            echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
-            exit 0
-        fi
-        if [ "${urltx}" != "" ]; then
-            sed -i "s/urltx_here/$urltx/g" ./nginxfiles/prodssltx.conf
-        else
-            echo
-            echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
-            exit 0
-        fi
-        if [ "${urlphp}" != "" ]; then
-            sed -i "s/urlprodphp_here/$urlphp/g" ./nginxfiles/prodssl.conf
-        else
-            echo
-            echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
-            exit 0
-        fi
-        if [ "${urlpanel}" != "" ]; then
-            sed -i "s/urlprodpanel_here/$urlpanel/g" ./nginxfiles/prodsslpanel.conf
-        else
-            echo
-            echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
-            exit 0
-        fi
-
-        if [ "${cfxport}" != "" ] && [ "${cfxport}" -ge 0 ] && [ "${cfxport}" -le 65535 ]; then
-            sed -i "s/ipprod_port_here/localhost:$cfxport/g" /etc/nginx/sites-enabled/prodssl.conf
-        else
-            echo
-            echo -e "\n${COLOR1}Port is empty, or wrong value [ 0 - 65535 ] ... ${NC}"
-            exit 0
-        fi      
-        if [ "${txport}" -ge 0 ] && [ "${txport}" -le 65535 ]; then
-            sed -i "s/iptxprod_port_here/localhost:$txport/g" /etc/nginx/sites-enabled/prodssltx.conf
-        else
-            echo -e "\n${COLOR1}Wrong txport number${NC} it must be 0 to 65535"
-            exit 0
-        fi
-
-        cp ./nginxfiles/nginx.conf /etc/nginx/nginx.conf
-        cp ./nginxfiles/default /etc/nginx/sites-available/default
-        cp ./nginxfiles/prodssl.conf /etc/nginx/sites-enabled/prodssl.conf
-        cp ./nginxfiles/prodssltx.conf /etc/nginx/sites-enabled/prodssltx.conf
-        cp ./nginxfiles/prodsslpanel.conf /etc/nginx/sites-enabled/prodsspanel.conf
-
-        echo
-        echo -e "\n${COLOR2}Configuration files done ! ... ${NC}"
-        echo
-        echo -e "\n${COLOR2} Start Nginx Server ... ${NC}"
-        systemctl start nginx
-        echo
+    if [ "${urlmachine}" != "" ]; then
+        sed -i "s/machine_url_here/$urlmachine/g" ./nginxfiles/default
     else
         echo
-        echo -e "\n${COLOR}Error generation SSL Certificates | Stop process ${NC}"  
+        echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
         exit 0
     fi
+    if [ "${urlplay}" != "" ]; then
+        sed -i "s/play_url_here/$urlplay/g" ./nginxfiles/prodssl.conf
+    else
+        echo
+        echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
+        exit 0
+    fi
+    if [ "${urltx}" != "" ]; then
+        sed -i "s/urltx_here/$urltx/g" ./nginxfiles/prodssltx.conf
+    else
+        echo
+        echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
+        exit 0
+    fi
+    if [ "${urlphp}" != "" ]; then
+        sed -i "s/urlprodphp_here/$urlphp/g" ./nginxfiles/prodssl.conf
+    else
+        echo
+        echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
+        exit 0
+    fi
+    if [ "${urlpanel}" != "" ]; then
+        sed -i "s/urlprodpanel_here/$urlpanel/g" ./nginxfiles/prodsslpanel.conf
+    else
+        echo
+        echo -e "\n${COLOR1}Url is empty, stop the process... ${NC}"
+        exit 0
+    fi
+
+    if [ "${cfxport}" != "" ] && [ "${cfxport}" -ge 0 ] && [ "${cfxport}" -le 65535 ]; then
+        sed -i "s/ipprod_port_here/localhost:$cfxport/g" /etc/nginx/sites-enabled/prodssl.conf
+    else
+        echo
+        echo -e "\n${COLOR1}Port is empty, or wrong value [ 0 - 65535 ] ... ${NC}"
+        exit 0
+    fi      
+    if [ "${txport}" -ge 0 ] && [ "${txport}" -le 65535 ]; then
+        sed -i "s/iptxprod_port_here/localhost:$txport/g" /etc/nginx/sites-enabled/prodssltx.conf
+    else
+        echo -e "\n${COLOR1}Wrong txport number${NC} it must be 0 to 65535"
+        exit 0
+    fi
+
+    cp ./nginxfiles/nginx.conf /etc/nginx/nginx.conf
+    cp ./nginxfiles/default /etc/nginx/sites-available/default
+    cp ./nginxfiles/prodssl.conf /etc/nginx/sites-enabled/prodssl.conf
+    cp ./nginxfiles/prodssltx.conf /etc/nginx/sites-enabled/prodssltx.conf
+    cp ./nginxfiles/prodsslpanel.conf /etc/nginx/sites-enabled/prodsspanel.conf
+
+    echo
+    echo -e "\n${COLOR2}Configuration files done ! ... ${NC}"
+    echo
+    echo -e "\n${COLOR2} Start Nginx Server ... ${NC}"
+    systemctl start nginx
     echo
     
 }
